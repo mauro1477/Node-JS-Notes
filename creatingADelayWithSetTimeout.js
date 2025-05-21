ace00a86ec5f759fd72bfc480c7a953be7718cc2
Mauro Vargas <maurovargas7725@gmail.com>
2025-05-21 13:45:54 -0600

    Created interval tracker need to stop program

--- creatingADelayWithSetTimeout.js
+++ creatingADelayWithSetTimeout.js
@@ -3,3 +3,13 @@ console.log(`Wait Time ${waitTime/1000} seconds`);
 const timerFinished = () => console.log("Done");
 
 setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+let currentTime = 0;
+
+const incrementInterval = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime/1000} seconds`);
+}
+
+const interval = setInterval(incrementInterval, waitInterval);
